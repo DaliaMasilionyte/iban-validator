a4127a47affd5bfe04757745a390bf76b6ba380f
DaliaMasilionyte <dmasilionyte@gmail.com>
2018-10-20 22:01:26 +0300

    fixed style

--- test/CheckDigitValidationTest.java
+++ test/CheckDigitValidationTest.java
@@ -8,17 +8,17 @@ class CheckDigitValidationTest {
             new PieceWiseCheckDigitValidation();
     private static final CheckDigitValidation FULL_CHECK_DIGIT_VALIDATION =
             new FullCheckDigitValidation();
-    private static final String validIban = "LT517044077788877777";
-    private static final String validIbanNumber = "7044077788877777212951";
+    private static final String VALID_IBAN = "LT517044077788877777";
+    private static final String VALID_IBAN_NUMBER = "7044077788877777212951";
 
     @Test
     void convertIbanToNumber() {
-        assertEquals(PIECE_WISE_CHECK_DIGIT_VALIDATION.convertIbanToNumber(validIban),
-                validIbanNumber);
+        assertEquals(PIECE_WISE_CHECK_DIGIT_VALIDATION.convertIbanToNumber(VALID_IBAN),
+                VALID_IBAN_NUMBER);
     }
     @Test
     void validateCheckDigitsPieceWise() {
-        assertTrue(PIECE_WISE_CHECK_DIGIT_VALIDATION.validateCheckDigits(validIbanNumber));
+        assertTrue(PIECE_WISE_CHECK_DIGIT_VALIDATION.validateCheckDigits(VALID_IBAN_NUMBER));
     }
     @Test
     void validateCheckDigitsInvalidPieceWise() {
@@ -27,7 +27,7 @@ class CheckDigitValidationTest {
 
     @Test
     void validateCheckDigitsFull() {
-        assertTrue(FULL_CHECK_DIGIT_VALIDATION.validateCheckDigits(validIbanNumber));
+        assertTrue(FULL_CHECK_DIGIT_VALIDATION.validateCheckDigits(VALID_IBAN_NUMBER));
     }
     @Test
     void validateCheckDigitsInvalidFull() {
